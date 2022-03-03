8a9b00a54818d1c5a078f634ed4f7d57132bbf66
sk193 <shahed.k123@gmail.com>
2022-03-03 16:01:21 -0600

    Update provider.tf

--- 02-vpc/provider.tf
+++ 02-vpc/provider.tf
@@ -4,8 +4,20 @@
 # https://cloud.ibm.com/docs/terraform?topic=terraform-provider-reference&-access-data-sources
 ##############################################################################
 
+#variable "ibmcloud_api_key" {
+#  description = "A valid API Key for a user or service ID in IBM Cloud."
+#}
+
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "~> 1.30.0"
+    }
+  }
+}
+
 provider "ibm" {
   region             = var.ibm_region
-  generation         = 2
-  ibmcloud_timeout   = 60
+#  ibmcloud_api_key = var.ibmcloud_api_key
 }
